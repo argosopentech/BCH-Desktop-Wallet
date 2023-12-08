d7b9bb09c79d438f4132c09b11a5e9f2f5376cc3
P.J. Finlay <pjappdevelopment@gmail.com>
2023-12-08 14:06:47 -0600

    Bounceback transaction

--- bchdesktopwallet/createtokensdemo.py
+++ bchdesktopwallet/createtokensdemo.py
@@ -1,6 +1,7 @@
 # createtokensdemo.py
 import bchdesktopwallet.localwalletman as localwalletman
 from bitcash import Key
+from time import sleep
 
 
 class TokenMan:
@@ -13,17 +14,18 @@ class TokenMan:
         helper_address = helper_key.address
 
         # Fund the helper wallet with 1000 satoshis
-        outputs = [(helper_address, 1e4, "satoshi")]
+        outputs = [(helper_address, 1e5, "satoshi")]
         self.key.send(outputs)
         print("Helper wallet funded.")
 
         # Send the 1000 satoshis back to the original wallet as an unspent genesis
-        outputs = [(self.key.address, int(1e4 * 0.9), "satoshi")]
+        outputs = [(self.key.address, int(1e5 * 0.9), "satoshi")]
         helper_key.send(outputs)
         print("Unspent genesis created.")
 
-        unspent = self.key.get_unspents()[-1]
-        print(unspent)
+        unspents = self.key.get_unspents()
+        unspent = unspents[-1]
+        print(unspents)
 
     def get_token_balance(self, token_name):
         return self.key.cashtoken_balance
@@ -36,7 +38,7 @@ class TokenMan:
 if __name__ == "__main__":
     wallet_manager = localwalletman.LocalWalletManager()
     wallets = wallet_manager.get_wallets()
-    for wallet in wallets:
-        print(f"Wallet: {wallet}")
-        tm = TokenMan(Key.from_int(wallet["private_key"]))
-        tm.create_token("test", "tst", 1000)
+    wallet = wallets[0]
+    print(f"Wallet: {wallet}")
+    tm = TokenMan(Key.from_int(wallet["private_key"]))
+    tm.create_token("test", "tst", 1000)
